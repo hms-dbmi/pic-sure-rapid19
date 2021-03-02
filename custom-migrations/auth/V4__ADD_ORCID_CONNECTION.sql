0a72ffa084030c4e072de8caba1e981664a68be7
Paul Avillach <paul_avillach@hms.harvard.edu>
2021-03-02 17:50:53 -0500

    add ORCHID SQL

---
+++ custom-migrations/auth/V4__ADD_ORCID_CONNECTION.sql
@@ -0,0 +1,3 @@
+use auth;
+INSERT INTO `connection` VALUES (unhex('972DCD8420F366FD00B090ED75D98F88'), 'ORCiD', 'orcid','orcid|','[{"label":"orcid", "id":"email"}]');
+INSERT INTO `userMetadataMapping` VALUES (unhex('16B0DD3620F0ED7998F8002DD847FC85'), '$.email', unhex('972DCD8420F366FD00B090ED75D98F88'), '$.email');
